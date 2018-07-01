fb57c8319013a263f020c326677ab42d2173ce2a
Mauro Cardoso Mortoni <mmortoni@hotmail.com>
2018-07-01 10:54:54 -0300

    Start Quiz score clear

--- src/components/StartQuizScreen/index.js
+++ src/components/StartQuizScreen/index.js
@@ -30,14 +30,10 @@ class StartQuizScreen extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { index: 0, question: '', correct: 0, lastQuestionAnswered: false }
+        this.state = { index: 0, lastQuestionAnswered: false }
         this.question = Array(this.props.navigation.state.params.cardDeck.length).fill(0);
 
         this.handleClick = this.handleClick.bind(this);
-    }
-
-    async componentDidMount() {
-        //       await this.props.getAllDeckData({ where: { parentId: this.props.navigation.state.params.quiz.id } });
     }
 
     async handleClick() {
@@ -45,8 +41,6 @@ class StartQuizScreen extends Component {
 
         if (this.state.index < cardDeck.length - 1) {
             this.setState({ index: this.state.index + 1 })
-
-            //if(this.state.question == 'Correct') this.setState({ correct: this.state.correct + 1 })
         }
     }
 
@@ -59,11 +53,9 @@ class StartQuizScreen extends Component {
 
     render() {
         const { quiz, cardDeck } = this.props.navigation.state.params;
-        //        const score = Math.round((this.state.correct / cardDeck.length) * 100) ;
         const score = this.question.reduce(
             (sum, value) => sum += value == 0 ? 1 : 0,
             0) / (this.question.length / 100);
-
 
         return (
             <Container>
@@ -93,7 +85,6 @@ class StartQuizScreen extends Component {
                                                 title: cardDeck[this.state.index].answer
                                             },
                                             buttonIndex => {
-                                                //this.setState({ question: BUTTONS[buttonIndex] });
                                                 this.question[this.state.index] = buttonIndex;
                                                 this.setState({ lastQuestionAnswered: this.state.index + 1 == cardDeck.length });
                                             }
